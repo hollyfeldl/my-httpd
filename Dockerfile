aefce3ee6bfb3113fb52f6fe1a0999ec37ed784f
Mark Rank <hollyfeldl@gmail.com>
2015-10-17 15:54:08 -0700

    Enabled configuration file and SSL for the site. Also added self-signed certs to it for now.
     Changes to be committed:
    	modified:   Dockerfile
    	modified:   build_markrank_net.sh
    	new file:   httpd-ssl.conf
    	new file:   httpd.conf

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,6 @@
 FROM httpd:latest
+COPY ./markrank.pem /usr/local/apache2/conf/markrank.pem
+COPY ./markrank-key.pem /usr/local/apache2/conf/markrank-key.pem
+COPY ./httpd.conf /usr/local/apache2/conf/httpd.conf
+COPY ./httpd-ssl.conf /usr/local/apache2/conf/extra/httpd-ssl.conf
 COPY ./http/ /usr/local/apache2/htdocs/ 
--- build_markrank_net.sh
+++ build_markrank_net.sh
@@ -8,23 +8,32 @@ echo "**********************************"
 
 # look for container name as the second parm and if not default it
 
+if [ -z $3 ] 
+then
+	curKeyPath="/home/mark/dev/private_keys"
+	echo "WARN - No path to the private SSL stuff, default to" $curKeyPath
+else
+	curContainer=$3
+	echo "INFO - Container set to" $curKeyPath
+fi
+
 if [ -z $2 ] 
 then
 	curContainer="my-httpd"
-	echo "No container provided, default to" $curContainer
+	echo "WARN - No container provided, default to" $curContainer
 else
 	curContainer=$2
-	echo "Container set to" $curContainer
+	echo "INFO - Container set to" $curContainer
 fi
 
 # look for the project id
 if [ -z $1 ] 
 then
 	curProject="my-container-httpd"
-	echo "No project provided, default to" $curProject
+	echo "WARN - No project provided, default to" $curProject
 else
 	curProject=$1
-	echo "Project set to" $curProject
+	echo "INFO - Project set to" $curProject
 fi
 
 # ask if we shoud continue
@@ -39,32 +48,64 @@ esac
 # look for the Dockerfile
 if test -e "./Dockerfile"
 then 
-	echo "Using local Dockerfile"
+	echo "INFO - Using local Dockerfile"
 	cat ./Dockerfile
 else
-	echo "Cannot find Dockerfile to build container"
+	echo "ERROR - Cannot find Dockerfile to build container"
 	exit 1
 fi
 
-echo "rebuild the container using the latest httpd" 
+# look for the DH Parms
+if test -e "$curKeyPath/dhparams_2048.pem"
+then 
+	echo "INFO - Found DH Parms file"
+	cp -v $curKeyPath/dhparams_2048.pem ./dhparams_2048.pem
+else
+	echo "ERROR - Cannot find DH Params file"
+	exit 1
+fi
+
+# look for the the private key
+if test -e "$curKeyPath/markrank-key.pem"
+then 
+	echo "INFO - Found the private key file"
+	cp -v $curKeyPath/markrank-key.pem ./markrank-key.pem
+else
+	echo "ERROR - Cannot find private key file file"
+	exit 1
+fi
+
+echo "INFO - Build a new cert with a 90 day life"
+openssl req -new -x509 -key ./markrank-key.pem -out ./markrank-part.pem -days 90 -nodes
+
+echo "INFO - Add on the unique DH Parms to the end of the cert"
+cat ./markrank-part.pem ./dhparams_2048.pem > ./markrank.pem
+
+echo "INFO - rebuild the container using the latest httpd" 
 docker build -t gcr.io/$curProject/$curContainer --pull=true .
 
 # some feedback
 docker images | grep $curContainer
 
-echo "push the container to gcloud repository"
+echo "INFO - push the container to gcloud repository"
 gcloud docker push gcr.io/$curProject/$curContainer
 
-echo "build the cluster"
+echo "INFO - build the cluster"
 gcloud container clusters create $curContainer-cluster --num-nodes 1 --machine-type g1-small
 
-echo "start a pod with the image"
-kubectl run $curContainer --image=gcr.io/$curProject/$curContainer --port=80
+echo "INFO - start a pod with the image on port 443"
+kubectl run $curContainer --image=gcr.io/$curProject/$curContainer --port=443
 
-echo "expose the service with an external load balancer"
+echo "INFO - expose the service with an external load balancer"
 kubectl expose rc $curContainer --create-external-load-balancer=true
 
-# some feedback
+echo "INFO - clean up the local cert info"
+rm -v ./markrank.pem
+rm -v ./markrank-key.pem
+rm -v ./markrank-part.pem
+rm -v ./dhparams_2048.pem
+
+echo "INFO - where is it running (kubectl get services)" 
 kubectl get services $curContainer
 
 # we are done
